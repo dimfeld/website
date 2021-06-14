b3e49429a71cfb5fa1c61eae42a80eb96b8474f4
Daniel Imfeld <daniel@imfeld.dev>
2021-06-14 08:35:44 +0000

    Import old teal color scheme

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,18 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        teal: colors.teal,
+        teal: {
+          50: '#edfafa',
+          100: '#d5f5f6',
+          200: '#afecef',
+          300: '#7edce2',
+          400: '#16bdca',
+          500: '#0694a2',
+          600: '#047481',
+          700: '#036672',
+          800: '#05505c',
+          900: '#014451',
+        },
         'cool-gray': colors.coolGray,
       },
       fontFamily: {
