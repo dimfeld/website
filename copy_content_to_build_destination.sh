853eeee52987e2a915ec2744b8162139c3de7a87
Daniel Imfeld <daniel@imfeld.dev>
2021-10-22 02:20:58 +0000

    copy wasm file into vercel endpoint

--- copy_content_to_build_destination.sh
+++ copy_content_to_build_destination.sh
@@ -7,5 +7,6 @@ export PATH=$(npm bin):${PATH}
 for OUT in "$LOCAL_BUILD_PATH" "$VERCEL_BUILD_PATH"; do
   copyfiles -u3 -e '**/*.ts' "src/lib/og-image/*" "$OUT"
   copyfiles "notes/*" "posts/*" "roam-pages/*" "$OUT"
+  cp node_modules/@dimfeld/create-social-card-wasm/create_social_card_wasm_bg.wasm "$OUT"
 done
 
