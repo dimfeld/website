787ae3f0a5238e83baf3de6e1b40ee10e5f2238a
Daniel Imfeld <daniel@imfeld.dev>
2023-10-02 22:22:20 -1000

    better headings with new export-logseq-note features

--- src/routes/journals/[year]/[month]/+page.svelte
+++ src/routes/journals/[year]/[month]/+page.svelte
@@ -1,10 +1,15 @@
 <script lang="ts">
+  import { page } from '$app/stores';
   import type { PageData } from './$types';
   import Journal from '$lib/Journal.svelte';
 
   export let data: PageData;
 </script>
 
+<h1 class="sr-only">
+  Journals for <time datetime="{$page.params.year}-{$page.params.month}"
+    >{$page.params.year}-{$page.params.month}</time>
+</h1>
 <div class="prose mt-2 flex w-full flex-col justify-center gap-8">
   {#each data.posts as journal}
     <Journal {...journal} />
--- src/routes/journals/[year]/[month]/[id]/+page.svelte
+++ src/routes/journals/[year]/[month]/[id]/+page.svelte
@@ -1,9 +1,14 @@
 <script lang="ts">
+  import { page } from '$app/stores';
   import Journal from '$lib/Journal.svelte';
   import type { PageData } from './$types';
 
   export let data: PageData;
 </script>
+
+<h1 class="sr-only">
+  Journals for <time datetime={$page.params.id}>{$page.params.id}</time>
+</h1>
 
 <div class="prose pt-2">
   <Journal {...data.post} showFooter />
