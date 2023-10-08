61db2d741cc6d623c905a51637b29e6cfa9b1984
Daniel Imfeld <daniel@imfeld.dev>
2023-10-08 13:11:56 -0400

    don't fetch script in dev mode

--- src/routes/plas/script.js/+server.ts
+++ src/routes/plas/script.js/+server.ts
@@ -1,7 +1,17 @@
+import { dev } from '$app/environment';
+
 export const config = {
   runtime: 'edge',
 };
 
 export function GET({ fetch }) {
+  if (dev) {
+    return new Response('', {
+      headers: {
+        'content-type': 'application/javascript',
+      },
+    });
+  }
+
   return fetch('https://plausible.io/js/script.js');
 }
